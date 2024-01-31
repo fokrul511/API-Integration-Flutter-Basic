77403903dec3518a3e71120c40a3f7703f599ad4
fokrul511 <islamfokrul2002@gmail.com>
2024-01-31 19:51:10 +0600

    inital

---
+++ lib/app.dart
@@ -0,0 +1,40 @@
+import 'package:crud_task_apps/task_list_screen.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.blue,
+          foregroundColor: Colors.white,
+        ),
+        inputDecorationTheme: InputDecorationTheme(
+          border: OutlineInputBorder(),
+          enabledBorder: OutlineInputBorder(),
+          focusedBorder: OutlineInputBorder(),
+          errorBorder: OutlineInputBorder(),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            padding: EdgeInsets.symmetric(
+              vertical: 18,
+              horizontal: 22,
+            ),
+            backgroundColor: Colors.amber,
+            foregroundColor: Colors.black,
+            textStyle: const TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+        ),
+        useMaterial3: true,
+      ),
+      home: const TaskListScreen(),
+    );
+  }
+}
